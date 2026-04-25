87db931d1871f93a4bf28973979404378121f150
rworron <rworron94@gmail.com>
2026-04-25 17:49:41 -0400

    temp

--- utils/fetchStandings.js
+++ utils/fetchStandings.js
@@ -1,19 +1,7 @@
-import config from "@/app/config";
-
-export async function fetchStandings(season = config.currentSeasonShort) {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    (process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000");
-
-  const res = await fetch(`${baseUrl}/api/standings/${season}`, {
+export async function fetchStandings(season) {
+  const res = await fetch(`http://localhost:3000/api/standings/${season}`, {
     next: { revalidate: 86400 },
   });
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch standings for ${season}`);
-  }
 
   return res.json();
 }
